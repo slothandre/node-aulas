76a8798310140d839ff0da976c33dc3f3b29297c
André Marques <andre.m.d.abreu@gmail.com>
2023-09-14 10:57:52 -0300

    Criação de um servidor com NodeJS

--- exemplo04/server.js
+++ exemplo04/server.js
@@ -1,20 +1,40 @@
-import http from "http";  // importando o módulo nativo http, mesmo sendo um módulo nativo, ainda assim precisamos do package.json
+import http from 'http'; // Importando módulo nativo http.
+import fs from 'fs/promises'; // file system: permite ao servidor acesso ao sistema de arquivos
 
-// criando uma função que vai monitorar as requisições e respostas do servidor.
-const monitorRequisicao = (req, res) => {
+// constante que monitora requisições e recebe dois parâmetros req (requisição) e res (resposta)
+const monitorRequisição = (req, res) => { // arrow function
+
     // Configurando o cabeçalho da resposta para trabalhar com arquivos HTML e charset-utf-8
     res.writeHead(200, {"Content-Type" : "text/html; charset=utf8"});
 
     // Avaliando cada requisição (a partir do link)
-    switch (req.url) {
-        case "/":
-            // o carregamento e leitura do index.html
-            break;
-        case "/sobre":
-            // o carregamento e leitura do sobre.html
-            break;
+    switch(req.url) {
+        case '/':
+            // fs faz o carregamento e leitura do arquivo
+            fs.readFile('pages/index.html')
+            // depois de pronto, envia o conteudo como resposta
+            .then(content => res.end(content));
+        break;
+
+        case '/sobre':
+            fs.readFile('pages/sobre.html')
+                .then(content => res.end(content));
+        break;
+
         default:
-            // o carregamento e leitura do 404.html
-            break;
-    }
-}
+            res.writeHead(404);
+            fs.readFile('pages/404.html')
+                .then(content => res.end(content));
+        break;
+    };
+};
+
+// Criando o servidor http e indicando a função de requisição
+const server = http.createServer(monitorRequisição);
+
+// iniciando a escuta do servidor local na porta 8080
+// server.listen(8080); // só esse trecho já seria o suficiente, mas é importante rodar a mensagem para ter certeza que tudo deu certo.
+
+server.listen(8080, () => {
+    console.log("Servidor node rodando normalmente");
+});
